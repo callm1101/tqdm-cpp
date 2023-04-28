6dc4eddbf42778da8f08dd1b0763bd0fd0e333b1
Yufei.Liu <Calm.Liu@outlook.com>
2023-04-28 20:43:07 +0800

    update example and readme

--- examples.cpp
+++ examples.cpp
@@ -10,12 +10,15 @@ int main()
     std::size_t N = 1000;
     Tqdm::ProgressBar bar(N);
 
-#pragma omp parallel for default(none) schedule(dynamic) shared(bar, N) num_threads(4)
+#pragma omp parallel for default(none) schedule(auto) shared(bar, N)           \
+  num_threads(4)
     for (std::size_t i = 0; i < N; i++)
     {
         std::this_thread::sleep_for(10ms);
 #pragma omp critical
-        bar.update();
+        {
+            bar.update();
+        }
     }
 
     return 0;
